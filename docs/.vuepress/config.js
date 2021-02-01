9b54337ed871617c9e8f7ac8b82f92af2e3a28ac
ziyang996 <ziyang996@outlook.com>
2021-02-01 16:40:10 +0800

    完成基本结构

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,33 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around'
+  title: 'JavaScript LeetCode',
+  description: '记录LeetCode的练习',
+  themeConfig: {
+    logo: 'https://www.vuepress.cn/hero.png',
+    nav: [
+      {
+        text: '简单',
+        link: '/views/easy/'
+      },
+      {
+        text: '中等',
+        link: '/views/medium/'
+      },
+      {
+        text: '困难',
+        link: '/views/hard/'
+      },
+      {
+        text: '学习网站',
+        items: [
+          { text: 'MDN Web 文档', link: 'https://developer.mozilla.org/zh-CN/' },
+          { text: '现代 JavaScript 教程', link: 'https://zh.javascript.info/' },
+        ]
+      }
+    ],
+    sidebar: {
+      '/views/easy/': ['1', '2'],
+      '/views/medium/': ['1', '2'],
+      '/views/hard/': ['1', '2'],
+    }
+  }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,9 @@
 ---
 home: true
+heroImage: https://static.leetcode-cn.com/cn-mono-assets/production/assets/leetcode-logo.5d9d9fa9.svg
+heroText: JavaScript LeetCode
+tagline: 记录LeetCode的练习
+actionText: 快速上手 →
+actionLink: /views/easy/
 ---
 :smile:
